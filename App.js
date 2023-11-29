e431c95e63d15204d89f8f1f6a1d2d4447b32129
kaulsalil88 <kaulsalil88@gmail.com>
2023-11-29 15:17:23 +0530

    Added search box, better error handling added loader

--- App.js
+++ App.js
@@ -1,44 +1,84 @@
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
 import { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSearchQuery = (query) => {
+    setSearchQuery(query);
+  };
 
   const fetchData = async (url) => {
     try {
       const response = await fetch(url);
       const json = await response.json();
+      setIsLoading(false);
       console.log(`json res:${JSON.stringify(json.results)}`);
       setData(json.results);
     } catch (error) {
+      setIsLoading(false);
       console.error(error);
     }
   };
 
   useEffect(() => {
+    setIsLoading(true);
     fetchData("https://randomuser.me/api/?results=20");
   }, []);
 
   return (
-    <ScrollView>
-      {data.map((item, idx) => {
-        return (
-          <View key={idx}>
-            <Image source={{ uri: item.picture.large }} style={styles.image} />
-            <View>
-              <Text style={styles.textName}>
-                {item.name.first} {item.name.last}
-              </Text>
-              <Text style={styles.textEmail}>{item.login.username}</Text>
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 20 }}>
+      <TextInput
+        placeHolder="Search"
+        clearButtonMode="always"
+        style={styles.searchBarStyle}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={searchQuery}
+        onChangeText={(query) => handleSearchQuery(query)}
+      />
+      <ScrollView>
+        {data.map((item, idx) => {
+          return (
+            <View key={idx}>
+              <Image
+                source={{ uri: item.picture.large }}
+                style={styles.image}
+              />
+              <View>
+                <Text style={styles.textName}>
+                  {item.name.first} {item.name.last}
+                </Text>
+                <Text style={styles.textEmail}>{item.login.username}</Text>
+              </View>
             </View>
-          </View>
-        );
-      })}
-    </ScrollView>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  searchBarStyle: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+  },
   textFriends: {
     fontSize: 20,
     textAlign: "left",
